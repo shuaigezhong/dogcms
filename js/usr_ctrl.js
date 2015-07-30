678ed4b8454bf9e4ce7fcce2376668e26d3329f8
shuaigezhong <80983543@qq.com>
2015-07-30 18:27:40 +0800

    2

--- js/usr_ctrl.js
+++ js/usr_ctrl.js
@@ -1,4 +1,12 @@
 //usr controller
-app.controller("usr_ctrl",function($scope){
+app.controller("usr_ctrl",function($scope,$http,$routeParams){
 	$scope.name = "lee";
+	$scope.title2 = "fff";
+	$scope.datas={};
+	console.dir($routeParams);
+	var p = $routeParams.id;
+	console.log(p)
+    $http.get("/admin/usr/list?tel=&age=&p=" + p).success(function(rep){
+		$scope.datas = rep.data.datas;
+	});
 })
